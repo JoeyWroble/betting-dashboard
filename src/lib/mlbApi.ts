e48953eb0eea8ff21857ac8e34d491cf9e9c6b65
JoeyWroble <jwroble@uiowa.edu>
2026-05-06 15:37:02 -0500

    Setting up API for MLB

--- src/lib/mlbApi.ts
+++ src/lib/mlbApi.ts
@@ -1,1 +1,19 @@
-export {};
+import axios from 'axios'; 
+
+const BASE_URL = 'https://statsapi.mlb.com/api/v1';
+
+// Function to get MLB games today
+export async function getTodaysGames() {
+    const today = new Date().toISOString().split('T')[0];
+    const response = await axios.get(`${BASE_URL}/schedule`, {
+        params: {
+            sportId: 1,
+            date: today,
+            hydrate: 'team, linescore, flags, liveLookin',
+        },
+    });
+
+    const dates = response.data.dates;
+    if (!dates || dates.length === 0) return [];
+    return dates[0].games;
+}
